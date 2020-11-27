0b0b3e69369da4b05b33a94a4bf00b1f807fcdeb
Efraimkin007 <69674425+Efraimkin007@users.noreply.github.com>
2020-11-27 21:15:49 +0700

    Create testFile_GraphQL.py

---
+++ testFile_GraphQL.py
@@ -0,0 +1,600 @@
+POST : 
+{
+  allPeople{
+    people{
+      name
+    }
+  }
+  allPlanets{
+    planets{
+      name
+    }
+  }
+  allFilms{
+    films{
+      title
+    }
+  }
+  allSpecies{
+    species{
+    name
+  }
+  }
+}
+
+Answer:
+{
+  "data": {
+    "allPeople": {
+      "people": [
+        {
+          "name": "Luke Skywalker"
+        },
+        {
+          "name": "C-3PO"
+        },
+        {
+          "name": "R2-D2"
+        },
+        {
+          "name": "Darth Vader"
+        },
+        {
+          "name": "Leia Organa"
+        },
+        {
+          "name": "Owen Lars"
+        },
+        {
+          "name": "Beru Whitesun lars"
+        },
+        {
+          "name": "R5-D4"
+        },
+        {
+          "name": "Biggs Darklighter"
+        },
+        {
+          "name": "Obi-Wan Kenobi"
+        },
+        {
+          "name": "Anakin Skywalker"
+        },
+        {
+          "name": "Wilhuff Tarkin"
+        },
+        {
+          "name": "Chewbacca"
+        },
+        {
+          "name": "Han Solo"
+        },
+        {
+          "name": "Greedo"
+        },
+        {
+          "name": "Jabba Desilijic Tiure"
+        },
+        {
+          "name": "Wedge Antilles"
+        },
+        {
+          "name": "Jek Tono Porkins"
+        },
+        {
+          "name": "Yoda"
+        },
+        {
+          "name": "Palpatine"
+        },
+        {
+          "name": "Boba Fett"
+        },
+        {
+          "name": "IG-88"
+        },
+        {
+          "name": "Bossk"
+        },
+        {
+          "name": "Lando Calrissian"
+        },
+        {
+          "name": "Lobot"
+        },
+        {
+          "name": "Ackbar"
+        },
+        {
+          "name": "Mon Mothma"
+        },
+        {
+          "name": "Arvel Crynyd"
+        },
+        {
+          "name": "Wicket Systri Warrick"
+        },
+        {
+          "name": "Nien Nunb"
+        },
+        {
+          "name": "Qui-Gon Jinn"
+        },
+        {
+          "name": "Nute Gunray"
+        },
+        {
+          "name": "Finis Valorum"
+        },
+        {
+          "name": "Padmé Amidala"
+        },
+        {
+          "name": "Jar Jar Binks"
+        },
+        {
+          "name": "Roos Tarpals"
+        },
+        {
+          "name": "Rugor Nass"
+        },
+        {
+          "name": "Ric Olié"
+        },
+        {
+          "name": "Watto"
+        },
+        {
+          "name": "Sebulba"
+        },
+        {
+          "name": "Quarsh Panaka"
+        },
+        {
+          "name": "Shmi Skywalker"
+        },
+        {
+          "name": "Darth Maul"
+        },
+        {
+          "name": "Bib Fortuna"
+        },
+        {
+          "name": "Ayla Secura"
+        },
+        {
+          "name": "Ratts Tyerel"
+        },
+        {
+          "name": "Dud Bolt"
+        },
+        {
+          "name": "Gasgano"
+        },
+        {
+          "name": "Ben Quadinaros"
+        },
+        {
+          "name": "Mace Windu"
+        },
+        {
+          "name": "Ki-Adi-Mundi"
+        },
+        {
+          "name": "Kit Fisto"
+        },
+        {
+          "name": "Eeth Koth"
+        },
+        {
+          "name": "Adi Gallia"
+        },
+        {
+          "name": "Saesee Tiin"
+        },
+        {
+          "name": "Yarael Poof"
+        },
+        {
+          "name": "Plo Koon"
+        },
+        {
+          "name": "Mas Amedda"
+        },
+        {
+          "name": "Gregar Typho"
+        },
+        {
+          "name": "Cordé"
+        },
+        {
+          "name": "Cliegg Lars"
+        },
+        {
+          "name": "Poggle the Lesser"
+        },
+        {
+          "name": "Luminara Unduli"
+        },
+        {
+          "name": "Barriss Offee"
+        },
+        {
+          "name": "Dormé"
+        },
+        {
+          "name": "Dooku"
+        },
+        {
+          "name": "Bail Prestor Organa"
+        },
+        {
+          "name": "Jango Fett"
+        },
+        {
+          "name": "Zam Wesell"
+        },
+        {
+          "name": "Dexter Jettster"
+        },
+        {
+          "name": "Lama Su"
+        },
+        {
+          "name": "Taun We"
+        },
+        {
+          "name": "Jocasta Nu"
+        },
+        {
+          "name": "R4-P17"
+        },
+        {
+          "name": "Wat Tambor"
+        },
+        {
+          "name": "San Hill"
+        },
+        {
+          "name": "Shaak Ti"
+        },
+        {
+          "name": "Grievous"
+        },
+        {
+          "name": "Tarfful"
+        },
+        {
+          "name": "Raymus Antilles"
+        },
+        {
+          "name": "Sly Moore"
+        },
+        {
+          "name": "Tion Medon"
+        }
+      ]
+    },
+    "allPlanets": {
+      "planets": [
+        {
+          "name": "Tatooine"
+        },
+        {
+          "name": "Alderaan"
+        },
+        {
+          "name": "Yavin IV"
+        },
+        {
+          "name": "Hoth"
+        },
+        {
+          "name": "Dagobah"
+        },
+        {
+          "name": "Bespin"
+        },
+        {
+          "name": "Endor"
+        },
+        {
+          "name": "Naboo"
+        },
+        {
+          "name": "Coruscant"
+        },
+        {
+          "name": "Kamino"
+        },
+        {
+          "name": "Geonosis"
+        },
+        {
+          "name": "Utapau"
+        },
+        {
+          "name": "Mustafar"
+        },
+        {
+          "name": "Kashyyyk"
+        },
+        {
+          "name": "Polis Massa"
+        },
+        {
+          "name": "Mygeeto"
+        },
+        {
+          "name": "Felucia"
+        },
+        {
+          "name": "Cato Neimoidia"
+        },
+        {
+          "name": "Saleucami"
+        },
+        {
+          "name": "Stewjon"
+        },
+        {
+          "name": "Eriadu"
+        },
+        {
+          "name": "Corellia"
+        },
+        {
+          "name": "Rodia"
+        },
+        {
+          "name": "Nal Hutta"
+        },
+        {
+          "name": "Dantooine"
+        },
+        {
+          "name": "Bestine IV"
+        },
+        {
+          "name": "Ord Mantell"
+        },
+        {
+          "name": "unknown"
+        },
+        {
+          "name": "Trandosha"
+        },
+        {
+          "name": "Socorro"
+        },
+        {
+          "name": "Mon Cala"
+        },
+        {
+          "name": "Chandrila"
+        },
+        {
+          "name": "Sullust"
+        },
+        {
+          "name": "Toydaria"
+        },
+        {
+          "name": "Malastare"
+        },
+        {
+          "name": "Dathomir"
+        },
+        {
+          "name": "Ryloth"
+        },
+        {
+          "name": "Aleen Minor"
+        },
+        {
+          "name": "Vulpter"
+        },
+        {
+          "name": "Troiken"
+        },
+        {
+          "name": "Tund"
+        },
+        {
+          "name": "Haruun Kal"
+        },
+        {
+          "name": "Cerea"
+        },
+        {
+          "name": "Glee Anselm"
+        },
+        {
+          "name": "Iridonia"
+        },
+        {
+          "name": "Tholoth"
+        },
+        {
+          "name": "Iktotch"
+        },
+        {
+          "name": "Quermia"
+        },
+        {
+          "name": "Dorin"
+        },
+        {
+          "name": "Champala"
+        },
+        {
+          "name": "Mirial"
+        },
+        {
+          "name": "Serenno"
+        },
+        {
+          "name": "Concord Dawn"
+        },
+        {
+          "name": "Zolan"
+        },
+        {
+          "name": "Ojom"
+        },
+        {
+          "name": "Skako"
+        },
+        {
+          "name": "Muunilinst"
+        },
+        {
+          "name": "Shili"
+        },
+        {
+          "name": "Kalee"
+        },
+        {
+          "name": "Umbara"
+        }
+      ]
+    },
+    "allFilms": {
+      "films": [
+        {
+          "title": "A New Hope"
+        },
+        {
+          "title": "The Empire Strikes Back"
+        },
+        {
+          "title": "Return of the Jedi"
+        },
+        {
+          "title": "The Phantom Menace"
+        },
+        {
+          "title": "Attack of the Clones"
+        },
+        {
+          "title": "Revenge of the Sith"
+        }
+      ]
+    },
+    "allSpecies": {
+      "species": [
+        {
+          "name": "Human"
+        },
+        {
+          "name": "Droid"
+        },
+        {
+          "name": "Wookie"
+        },
+        {
+          "name": "Rodian"
+        },
+        {
+          "name": "Hutt"
+        },
+        {
+          "name": "Yoda's species"
+        },
+        {
+          "name": "Trandoshan"
+        },
+        {
+          "name": "Mon Calamari"
+        },
+        {
+          "name": "Ewok"
+        },
+        {
+          "name": "Sullustan"
+        },
+        {
+          "name": "Neimodian"
+        },
+        {
+          "name": "Gungan"
+        },
+        {
+          "name": "Toydarian"
+        },
+        {
+          "name": "Dug"
+        },
+        {
+          "name": "Twi'lek"
+        },
+        {
+          "name": "Aleena"
+        },
+        {
+          "name": "Vulptereen"
+        },
+        {
+          "name": "Xexto"
+        },
+        {
+          "name": "Toong"
+        },
+        {
+          "name": "Cerean"
+        },
+        {
+          "name": "Nautolan"
+        },
+        {
+          "name": "Zabrak"
+        },
+        {
+          "name": "Tholothian"
+        },
+        {
+          "name": "Iktotchi"
+        },
+        {
+          "name": "Quermian"
+        },
+        {
+          "name": "Kel Dor"
+        },
+        {
+          "name": "Chagrian"
+        },
+        {
+          "name": "Geonosian"
+        },
+        {
+          "name": "Mirialan"
+        },
+        {
+          "name": "Clawdite"
+        },
+        {
+          "name": "Besalisk"
+        },
+        {
+          "name": "Kaminoan"
+        },
+        {
+          "name": "Skakoan"
+        },
+        {
+          "name": "Muun"
+        },
+        {
+          "name": "Togruta"
+        },
+        {
+          "name": "Kaleesh"
+        },
+        {
+          "name": "Pau'an"
+        }
+      ]
+    }
+  }
+}
